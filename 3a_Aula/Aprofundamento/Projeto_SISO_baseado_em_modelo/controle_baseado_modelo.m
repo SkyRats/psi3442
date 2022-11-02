1c9347f6da79f48cf84f676c36e920f411c8bfae
Fernando Zolubas Preto <49964943+Zolubas@users.noreply.github.com>
2022-11-01 22:36:05 -0300

    Add files via upload

---
+++ 3a_Aula/Aprofundamento/Projeto_SISO_baseado_em_modelo/controle_baseado_modelo.m
@@ -0,0 +1,66 @@
+fs = 40; %Hz
+Ts = 1/fs;
+
+%Drone ============================================
+s = tf('s');
+z = tf('z',Ts);
+G = 1/(s*(s+1));
+
+
+Gd = c2d(G,Ts,'zoh');
+
+poleGd = pole(Gd);
+Gd = zpk(minreal(Gd));
+[zg,pg,kg] = zpkdata(Gd);
+
+%Projeto do Controlador PID ========================
+%Cd = K*(z-c1)*(z-c2)/(z*(z-1))
+
+
+%Sobressinal = Mp
+%Acomodacao de 2% em 8s
+
+Mp = 3;%
+
+xi = -log(Mp/100)/sqrt(pi^2 + log(Mp/100)^2); %garante Sobressinal nulo
+%ts = 4/xi*wn
+ts = 10;
+wn = 4/(xi*ts);
+
+s1 = -xi*wn + 1i*wn*sqrt(1-xi^2);
+s2 = -xi*wn - 1i*wn*sqrt(1-xi^2);
+
+b = 2/Ts;
+
+A = [1  -1       0
+     b  -2*xi*wn 0
+     0   wn*wn  -b];
+v = [2*xi*wn-b
+     wn*wn
+     0];
+ 
+aux = A\v; 
+
+pc = aux(1);
+p = aux(2);
+k = aux(3);
+
+zc = pg{1}(2);
+
+Cc = k*(s + zc)/(s + pc);
+
+%step(feedback(G*Cc,1));
+
+Cd = c2d(Cc,Ts,'tustin');
+
+FTMF_continuo = feedback(G*Cc,1);
+FTMF_digital = feedback(Gd*Cd,1);
+
+step(FTMF_continuo,FTMF_digital);
+
+[zcd,pcd,kcd] = zpkdata(Cd);
+
+%%
+disp('Controlador em python')
+controllaw = ['un = ',num2str(-pcd{1}), '*un_1 +', num2str(k), '*en +',num2str(k*(zcd{1})) ,'*en_1'];
+disp(controllaw)
